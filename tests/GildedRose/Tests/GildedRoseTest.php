8d1f25cd1673aa374b466eea4eaa2eb9afd9e705
Rocco Palladino <rpalladino@gmail.com>
2017-02-25 00:35:28 -0600

    Add unit tests for aged brie

--- tests/GildedRose/Tests/GildedRoseTest.php
+++ tests/GildedRose/Tests/GildedRoseTest.php
@@ -40,6 +40,62 @@ class GildedRoseTest extends TestCase
         $this->assertEquals(0, $item->quality);
     }
 
+    public function testUpdateQualityOfAgedBrieBeforeSellDate()
+    {
+        $item = new Item(['name' => 'Aged Brie', 'sellIn' => 5, 'quality' => 10]);
+        $this->runProgramWith($item);
+        $this->assertEquals(4, $item->sellIn);
+        $this->assertEquals(11, $item->quality);
+    }
+
+    public function testUpdateQualityOfAgedBrieBeforeSellDateWithMaxQuality()
+    {
+        $item = new Item(['name' => 'Aged Brie', 'sellIn' => 5, 'quality' => 50]);
+        $this->runProgramWith($item);
+        $this->assertEquals(4, $item->sellIn);
+        $this->assertEquals(50, $item->quality);
+    }
+
+    public function testUpdateQualityOfAgedBrieOnSellDate()
+    {
+        $item = new Item(['name' => 'Aged Brie', 'sellIn' => 0, 'quality' => 10]);
+        $this->runProgramWith($item);
+        $this->assertEquals(-1, $item->sellIn);
+        $this->assertEquals(12, $item->quality);
+    }
+
+    public function testUpdateQualityOfAgedBrieOnSellDateNearMaxQuality()
+    {
+        $item = new Item(['name' => 'Aged Brie', 'sellIn' => 0, 'quality' => 49]);
+        $this->runProgramWith($item);
+        $this->assertEquals(-1, $item->sellIn);
+        $this->assertEquals(50, $item->quality);
+    }
+
+    public function testUpdateQualityOfAgedBrieOnSellDateWithMaxQuality()
+    {
+        $item = new Item(['name' => 'Aged Brie', 'sellIn' => 0, 'quality' => 50]);
+        $this->runProgramWith($item);
+        $this->assertEquals(-1, $item->sellIn);
+        $this->assertEquals(50, $item->quality);
+    }
+
+    public function testUpdateQualityOfAgedBrieAfterSellDate()
+    {
+        $item = new Item(['name' => 'Aged Brie', 'sellIn' => -5, 'quality' => 8]);
+        $this->runProgramWith($item);
+        $this->assertEquals(-6, $item->sellIn);
+        $this->assertEquals(10, $item->quality);
+    }
+
+    public function testUpdateQualityOfAgedBrieAfterSellDateWithMaxQuality()
+    {
+        $item = new Item(['name' => 'Aged Brie', 'sellIn' => -5, 'quality' => 50]);
+        $this->runProgramWith($item);
+        $this->assertEquals(-6, $item->sellIn);
+        $this->assertEquals(50, $item->quality);
+    }
+
     private function runProgramWith(Item $item)
     {
         $sut = new Program([$item]);

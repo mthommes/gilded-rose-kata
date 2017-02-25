818a9df19606f40dfdb1b458b33ad771045873a0
Rocco Palladino <rpalladino@gmail.com>
2017-02-24 23:21:39 -0600

    Add a characterization test

--- tests/GildedRose/Tests/GildedRoseTest.php
+++ tests/GildedRose/Tests/GildedRoseTest.php
@@ -3,7 +3,14 @@
 namespace GildedRose\Tests;
 
 use PHPUnit\Framework\TestCase;
+use GildedRose\Program;
 
 class GildedRoseTest extends TestCase
 {
+    public function testItPrintsAllItemsFromMain()
+     {
+         $output = file_get_contents(__DIR__ . '/output_fixture.txt');
+         $this->expectOutputString($output);
+         $program = Program::main(100);
+     }
 }

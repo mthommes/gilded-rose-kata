601038eb000d8f020dd65748eff2c42db16b866d
Matt Thommes <mthommes@activecampaign.com>
2017-03-14 11:38:37 -0500

    Starting refactoring.

--- src/GildedRose/Program.php
+++ src/GildedRose/Program.php
@@ -45,91 +45,78 @@ namespace GildedRose;
  * 50, however "Sulfuras" is a legendary item and as such its Quality is 80 and
  * it never alters.
  */
-class Program
+
+class QualityCalculator
+{
+	public function update($items) {
+		for ($i = 0; $i < count($items); $i++) {
+				if ($items[$i]->name != "Aged Brie" && $items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
+						if ($items[$i]->quality > 0) {
+								if ($items[$i]->name != "Sulfuras, Hand of Ragnaros") {
+										$items[$i]->quality = $items[$i]->quality - 1;
+								}
+						}
+				} else {
+						if ($items[$i]->quality < 50) {
+								$items[$i]->quality = $items[$i]->quality + 1;
+
+								if ($items[$i]->name == "Backstage passes to a TAFKAL80ETC concert") {
+										if ($items[$i]->sellIn < 11) {
+												if ($items[$i]->quality < 50) {
+														$items[$i]->quality = $items[$i]->quality + 1;
+												}
+										}
+
+										if ($items[$i]->sellIn < 6) {
+												if ($items[$i]->quality < 50) {
+														$items[$i]->quality = $items[$i]->quality + 1;
+												}
+										}
+								}
+						}
+				}
+
+				if ($items[$i]->name != "Sulfuras, Hand of Ragnaros") {
+						$items[$i]->sellIn = $items[$i]->sellIn - 1;
+				}
+
+				if ($items[$i]->sellIn < 0) {
+						if ($items[$i]->name != "Aged Brie") {
+								if ($items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
+										if ($items[$i]->quality > 0) {
+												if ($items[$i]->name != "Sulfuras, Hand of Ragnaros") {
+														$items[$i]->quality = $items[$i]->quality - 1;
+												}
+										}
+								} else {
+										$items[$i]->quality = $items[$i]->quality - $items[$i]->quality;
+								}
+						} else {
+								if ($items[$i]->quality < 50) {
+										$items[$i]->quality = $items[$i]->quality + 1;
+								}
+						}
+				}
+		}
+		return $items;
+	}
+}
+
+class Program extends QualityCalculator
 {
     private $items = array();
 
-    public static function Main($days = 1)
-    {
-        echo "OMGHAI!\n";
+    public function __construct(array $items, $days = 1) {
+        $this->items = $items;
 
-        $app = new Program(array(
-              new Item(array( 'name' => "+5 Dexterity Vest",'sellIn' => 10,'quality' => 20)),
-              new Item(array( 'name' => "Aged Brie",'sellIn' => 2,'quality' => 0)),
-              new Item(array( 'name' => "Elixir of the Mongoose",'sellIn' => 5,'quality' => 7)),
-              new Item(array( 'name' => "Sulfuras, Hand of Ragnaros",'sellIn' => 0,'quality' => 80)),
-              new Item(array(
-                     'name' => "Backstage passes to a TAFKAL80ETC concert",
-                     'sellIn' => 15,
-                     'quality' => 20
-              )),
-              new Item(array('name' => "Conjured Mana Cake",'sellIn' => 3,'quality' => 6)),
-        ));
+				echo "OMGHAI!\n";
 
         for ($i = 1; $i <= $days; $i++) {
-            $app->UpdateQuality();
+            $this->items = $this->update($this->items);
             echo "-------- day $i --------\n";
             echo sprintf("%50s - %7s - %7s\n", "Name", "SellIn", "Quality");
-            foreach ($app->items as $item) {
+            foreach ($this->items as $item) {
                 echo sprintf("%50s - %7d - %7d\n", $item->name, $item->sellIn, $item->quality);
-            }
-        }
-    }
-
-    public function __construct(array $items)
-    {
-        $this->items = $items;
-    }
-
-    public function UpdateQuality()
-    {
-        for ($i = 0; $i < count($this->items); $i++) {
-            if ($this->items[$i]->name != "Aged Brie" && $this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
-                if ($this->items[$i]->quality > 0) {
-                    if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                        $this->items[$i]->quality = $this->items[$i]->quality - 1;
-                    }
-                }
-            } else {
-                if ($this->items[$i]->quality < 50) {
-                    $this->items[$i]->quality = $this->items[$i]->quality + 1;
-
-                    if ($this->items[$i]->name == "Backstage passes to a TAFKAL80ETC concert") {
-                        if ($this->items[$i]->sellIn < 11) {
-                            if ($this->items[$i]->quality < 50) {
-                                $this->items[$i]->quality = $this->items[$i]->quality + 1;
-                            }
-                        }
-
-                        if ($this->items[$i]->sellIn < 6) {
-                            if ($this->items[$i]->quality < 50) {
-                                $this->items[$i]->quality = $this->items[$i]->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                $this->items[$i]->sellIn = $this->items[$i]->sellIn - 1;
-            }
-
-            if ($this->items[$i]->sellIn < 0) {
-                if ($this->items[$i]->name != "Aged Brie") {
-                    if ($this->items[$i]->name != "Backstage passes to a TAFKAL80ETC concert") {
-                        if ($this->items[$i]->quality > 0) {
-                            if ($this->items[$i]->name != "Sulfuras, Hand of Ragnaros") {
-                                $this->items[$i]->quality = $this->items[$i]->quality - 1;
-                            }
-                        }
-                    } else {
-                        $this->items[$i]->quality = $this->items[$i]->quality - $this->items[$i]->quality;
-                    }
-                } else {
-                    if ($this->items[$i]->quality < 50) {
-                        $this->items[$i]->quality = $this->items[$i]->quality + 1;
-                    }
-                }
             }
         }
     }
--- tests/GildedRose/Tests/GildedRoseTest.php
+++ tests/GildedRose/Tests/GildedRoseTest.php
@@ -3,14 +3,33 @@
 namespace GildedRose\Tests;
 
 use PHPUnit\Framework\TestCase;
+use GildedRose\Item;
 use GildedRose\Program;
 
 class GildedRoseTest extends TestCase
 {
-    public function testItPrintsAllItemsFromMain()
-     {
-         $output = file_get_contents(__DIR__ . '/output_fixture.txt');
-         $this->expectOutputString($output);
-         $program = Program::main(100);
-     }
+	private $items = array();
+
+	public function __construct()
+	{
+		$this->items[] = new Item(array('name' => "+5 Dexterity Vest",'sellIn' => 10,'quality' => 20));
+		$this->items[] = new Item(array('name' => "Aged Brie",'sellIn' => 2,'quality' => 0));
+		$this->items[] = new Item(array('name' => "Elixir of the Mongoose",'sellIn' => 5,'quality' => 7));
+		$this->items[] = new Item(array('name' => "Sulfuras, Hand of Ragnaros",'sellIn' => 0,'quality' => 80));
+		$this->items[] = new Item(array('name' => "Backstage passes to a TAFKAL80ETC concert", 'sellIn' => 15, 'quality' => 20));
+		$this->items[] = new Item(array('name' => "Conjured Mana Cake",'sellIn' => 3,'quality' => 6));
+	}
+
+	public function testItPrintsAllItemsFromMain()
+	{
+		$output = file_get_contents(__DIR__ . '/output_fixture.txt');
+		$this->expectOutputString($output);
+		$program = new Program($this->items, 100);
+	}
+
+	public function testQualityDegradation()
+	{
+		//$program = Program::main(2);
+		//$this->assertEquals(1, 0);
+	}	
 }
